b8344f1c79e86542d3a7caa9b53e254aee0a7dea
Pavel Sushko <nintsao@gmail.com>
2023-10-07 17:48:19 +0300

    added features, fixed bugs

--- laba_1/src/group5/varC/sushko/products/Main.java
+++ laba_1/src/group5/varC/sushko/products/Main.java
@@ -3,8 +3,8 @@ package group5.varC.sushko.products;
 import group5.varC.sushko.products.Dishes.Breakfast;
 import group5.varC.sushko.products.Components.Bread;
 import group5.varC.sushko.products.Components.Milk;
-
-// a,в,
+//branch laba1varC
+// a,б,в,г,
 public class Main {
     public static void main(String[] args){
         Bread bread = new Bread(13);
@@ -12,8 +12,23 @@ public class Main {
         Breakfast breakfast = new Breakfast();
         breakfast.setCompound(bread);
         breakfast.setCompound(milk);
-        System.out.println("calories in Breakfast:" + breakfast.calculateCalories());
         breakfast.ProductConsistOf();
         breakfast.checkNumOfRepeatedProducts(new Milk(120));
+//        if (args.length == 0) {
+//            System.out.println("NO ARGS , INPUT THIS COMMANDS FROM ROOT DIRECTORY javac group5/varC/sushko/products/Main.java \n" +
+//                    "java group5.varC.sushko.products.Main args");
+//        } else {
+//            if (args[0].equals("-calories"))
+//                System.out.println("Calories in breakfast: " + breakfast.calculateCalories());
+//        }
+        try {
+            if (args[0].equals("-calories"))
+                System.out.println("calories in Breakfast:" + breakfast.calculateCalories());
+        }
+        catch (ArrayIndexOutOfBoundsException exception){
+            System.out.println("NO ARGS , INPUT THIS COMMANDS FROM ROOT DIRECTORY javac group5/varC/sushko/products/Main.java \n" +
+                    "java group5.varC.sushko.products.Main args");
+        }
+
     }
 }
